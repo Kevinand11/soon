790496a9108ab321f609b7a1c9bea1dc47ddba15
Kevin Izuchukwu <kevinfizu@gmail.com>
2021-04-02 11:53:30 +0100

    push test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <img alt="Stranerd Logo" src="./assets/stranerd_logo.svg" height="50">
-    <h2>Website Launching Soon</h2>
+    <h2>Website Launching Soon!</h2>
     <p>
         We are building you a platform where you have access to learn from NERDS.
         <br>
